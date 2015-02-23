97a1ad8f1826e04457b29ab3a5f3bffddf29546f
rafal <rafalpedziwiatr1@gmail.com>
2015-02-23 22:47:43 +0100

    usunięcie głupiego komentarza

--- SpisekUBociana/SpisekUBociana/Game1.cs
+++ SpisekUBociana/SpisekUBociana/Game1.cs
@@ -11,9 +11,6 @@ using Microsoft.Xna.Framework.Media;
 
 namespace SpisekUBociana
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
